55899f0930db80cb992418c91b768b098c5fdde4
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-04-17 09:05:43 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='collective.linguasitemap',
       version=version,
       description="Sitemap for MultiLanguage Plone site using LinguaPlone",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
